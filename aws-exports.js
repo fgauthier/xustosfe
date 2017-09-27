298cbdcbd4bfa12a14b7e7df41f3f4c02b70f6f0
Francois Gauthier <francois.gauthier@ieee.org>
2017-09-27 16:49:09 -0700

    Delete aws-exports.js

--- aws-exports.js
+++
@@ -1,6 +0,0 @@
-export default awsmobile = {
-  aws_user_pools_id: 'us-east-1_3bjiw6GWN',
-  aws_user_pools_web_client_id: 'rjuicbqosc64po7gkc4javo40',
-  aws_cognito_identity_pool_id: 'us-east-1:9e9d550f-5f53-475a-b38a-cd30efc8bf9a',
-  aws_project_region: 'us-east-1'
-};
